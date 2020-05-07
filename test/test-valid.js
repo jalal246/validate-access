effe2c95bcbc2b4ec364309f9fa949ee617c2b4e
jalal246 <jimmy002020@gmail.com>
2020-05-08 00:03:25 +0200

    Add new test

--- test/test-valid.js
+++ test/test-valid.js
@@ -115,6 +115,37 @@ describe("valid", () => {
     });
   });
 
+  it("figures out entries with src included or extensions attached", () => {
+    const filePath = resolve(source, "valid-json-entries-src");
+
+    const res = validateAccess({
+      dir: filePath,
+      entry: ["src/b", "src/a.js", "src/index.ts"],
+    });
+
+    expect(res).to.deep.equal({
+      isJsonValid: true,
+      isSrc: true,
+      isEntryValid: [
+        {
+          entry: "b",
+          entryExt: "ts",
+          isValid: true,
+        },
+        {
+          entry: "a",
+          entryExt: "js",
+          isValid: true,
+        },
+        {
+          entry: "index",
+          entryExt: "ts",
+          isValid: false,
+        },
+      ],
+    });
+  });
+
   it("valid json custom entries-lib different ext", () => {
     const filePath = resolve(source, "valid-json-entry-lib");
 
