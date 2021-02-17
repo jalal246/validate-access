0c1d7ec2814059b7c6471dab4cf3c4dc04450e14
jalal246 <jimmy002020@gmail.com>
2021-02-17 08:02:14 +0200

    fixing entryext

--- test/test-invalid.js
+++ test/test-invalid.js
@@ -1,55 +1,55 @@
-// /* eslint-disable import/no-extraneous-dependencies */
-// const { expect } = require("chai");
-// const { resolve } = require("path");
+/* eslint-disable import/no-extraneous-dependencies */
+const { expect } = require("chai");
+const { resolve } = require("path");
 
-// const { validateAccess } = require("../lib");
+const { validateAccess } = require("../lib");
 
-// const source = resolve(__dirname, "fixtures");
+const source = resolve(__dirname, "fixtures");
 
-// describe("invalid", () => {
-//   it("invalid json", () => {
-//     const filePath = resolve(source, "invalid-json");
+describe("invalid", () => {
+  it("invalid json", () => {
+    const filePath = resolve(source, "invalid-json");
 
-//     const res = validateAccess({ dir: filePath });
+    const res = validateAccess({ dir: filePath });
 
-//     expect(res).to.deep.equal({
-//       isJsonValid: false,
-//       isSrc: false,
-//       entry: "index",
-//       isEntryValid: true,
-//       entryExt: "js",
-//     });
-//   });
+    expect(res).to.deep.equal({
+      isJsonValid: false,
+      isSrc: false,
+      entry: "index",
+      isEntryValid: true,
+      entryExt: "js",
+    });
+  });
 
-//   it("invalid json entries-flat", () => {
-//     const filePath = resolve(source, "invalid-entries-flat");
+  it("invalid json entries-flat", () => {
+    const filePath = resolve(source, "invalid-entries-flat");
 
-//     const res = validateAccess({
-//       dir: filePath,
-//     });
+    const res = validateAccess({
+      dir: filePath,
+    });
 
-//     expect(res).to.deep.equal({
-//       isJsonValid: true,
-//       isSrc: false,
-//       entry: "index",
-//       isEntryValid: false,
-//       entryExt: null,
-//     });
-//   });
+    expect(res).to.deep.equal({
+      isJsonValid: true,
+      isSrc: false,
+      entry: "index",
+      isEntryValid: false,
+      entryExt: "",
+    });
+  });
 
-//   it("invalid json entries-src", () => {
-//     const filePath = resolve(source, "invalid-entries-src");
+  it("invalid json entries-src", () => {
+    const filePath = resolve(source, "invalid-entries-src");
 
-//     const res = validateAccess({
-//       dir: filePath,
-//     });
+    const res = validateAccess({
+      dir: filePath,
+    });
 
-//     expect(res).to.deep.equal({
-//       isJsonValid: true,
-//       isSrc: false,
-//       entry: "index",
-//       isEntryValid: false,
-//       entryExt: null,
-//     });
-//   });
-// });
+    expect(res).to.deep.equal({
+      isJsonValid: true,
+      isSrc: false,
+      entry: "index",
+      isEntryValid: false,
+      entryExt: "",
+    });
+  });
+});
